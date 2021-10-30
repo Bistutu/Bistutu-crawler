623351fb2b8de9990ae5ee78403747f515b00461
dekang <1761477584@qq.com>
2021-10-30 14:11:51 +0800

    deemm

--- src/main/java/Cookie.java
+++ src/main/java/Cookie.java
@@ -1,9 +1,8 @@
 
 public class Cookie {
 	public static String get() {
-		String cookieData=
-"Cookie: _ht=person1; EMAP_LANG=zh; _WEU=h0_*SM1gdXp_Lc_Yicxi7wpipQ6IvUhmJ7Dh*LNgcfh7S8HJk2eumlNwLCu5p7Ur3O7Yfjhm2JzLA_5lnFfAitsBhKukfNJgsxWoo42*g7Faw38m770yQJlxNFnQoeHggf6foCHMHoQQfbvErhUZbagvE_IO8_Ub0X0BaWWIA04gosWgdLSsXfXYJ8drrhqmG5UCjeZ74Jo.; route=c83e11eebb8da619ef6a94bc26688b29; MOD_AUTH_CAS=MOD_AUTH_ST-246579-NZSXKkE-kefZT2YxemqmQvXUxC0localhost; JSESSIONID=y1Nfh8Lb0lyC8dvGr5ym56sNxLT1v5j10pnd8ZXGLVLFbgnk2GLW!-1241296154"				;
-		cookieData=cookieData.substring(cookieData.indexOf("_ht"),cookieData.length());
+		String cookieData="Cookie: _ht=person1; EMAP_LANG=zh; _WEU=bbiOHjFlYBLMo6LmRmuAANJHQ*c5aM1h4ALOoGpVHGdPLBs_wJm4h0kGNggqJMaCBNQeKup3o2C6oa4vqqXkC9P1x*JtDA2BCy8BoSbrDEvkbPH9N3FAeLN2wdTpHq9y0hcTgcneF4ppwiaqAumrsi94euAhs82p5WWDAvti4yErysqyQ1IFk9_3TNUqyzMQY5bu3HxYivKkg7B5HDhtdHo5HFFA0hff; route=c83e11eebb8da619ef6a94bc26688b29; JSESSIONID=RbzTh8GVK8KL1DTMlbCHQ5hyjvbPbnfhGrGSdnnqWzLchLvZhCGg!1961833876; MOD_AUTH_CAS=MOD_AUTH_ST-246821-uw13CxOK2oEJiX7R1-TPp7uGSZslocalhost";
+				cookieData=cookieData.substring(cookieData.indexOf("_ht"),cookieData.length());
 		return cookieData;
 	}
 }
--- src/main/java/JXQ.java
+++ src/main/java/JXQ.java
@@ -20,7 +20,7 @@ public class JXQ {
 			date = "2021-"+month+"-" + day;
 			time = 0;
 			int colorNumber=0;
-			printWriter = new PrintWriter(new File("/Users/thinkstu/Desktop/emptyData/2/2"+month + day + ".json"));
+			printWriter = new PrintWriter(new File("C:/Users/康/Desktop/空教室数据/2/2"+month + day + ".json"));
 			printWriter.print("[");
 			
 			while (time < 1000) {
